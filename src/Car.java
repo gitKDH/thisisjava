80a73b9d394688c918e5c8d0790bf0c926e8ecb3
gitKDH <reg0212@naver.com>
2023-08-20 00:34:06 +0900

    car example 수정2

--- src/Car.java
+++ src/Car.java
@@ -1,21 +1,45 @@
 public class Car {
+    int gas;
     String company = "현대자동차";
-    String model = "그랜저";
-    String color = "검은색";
-    int speed;
-    int maxSpeed = 350;
-    boolean start;
-    Tire tire;
+    String model;
+    String color;
+    int maxSpeed;
 
-    Car(){}
+    public Car() {
+
+    }
+
+    void setGas(int gas){
+        this.gas = gas;
+    }
+
+    boolean isLeftGas(){
+        if(gas == 0){
+            System.out.println("gas가 없다");
+            return false;
+        }
+        System.out.println("gas있음");
+        return true;
+    }
+
+    void run(){
+        while(true){
+            if(gas > 0){
+                System.out.println("달린다. 잔량 : "+ gas);
+                gas-=1;
+            }else{
+                System.out.println("멈춘다. 잔량 : " + gas);
+                return;
+            }
+        }
+    }
 
     Car(String model){
-        this.model = model;
+        this(model, "은색",250);
     }
 
     Car(String model, String color){
-        this.model = model;
-        this.color = color;
+        this(model,color,250);
     }
 
     Car(String model, String color, int maxSpeed){
