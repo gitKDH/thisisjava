4df6f500b3221e13c45023f0f4a35e7acc58b343
gitKDH <reg0212@naver.com>
2023-08-20 22:53:51 +0900

    chapter6 마무리

--- src/Car.java
+++ src/Car.java
@@ -1,50 +1,75 @@
 public class Car {
-    int gas;
-    String company = "현대자동차";
-    String model;
-    String color;
-    int maxSpeed;
 
-    public Car() {
+    private int speed;
+    private boolean stop;
 
+    public int getSpeed() {
+        return speed;
     }
 
-    void setGas(int gas){
-        this.gas = gas;
-    }
-
-    boolean isLeftGas(){
-        if(gas == 0){
-            System.out.println("gas가 없다");
-            return false;
-        }
-        System.out.println("gas있음");
-        return true;
-    }
-
-    void run(){
-        while(true){
-            if(gas > 0){
-                System.out.println("달린다. 잔량 : "+ gas);
-                gas-=1;
-            }else{
-                System.out.println("멈춘다. 잔량 : " + gas);
-                return;
-            }
+    public void setSpeed(int speed) {
+        if(speed < 0){
+            this.speed = 0;
+            return;
+        }else{
+            this.speed = speed;
         }
     }
 
-    Car(String model){
-        this(model, "은색",250);
+    public boolean isStop() {
+        return stop;
     }
 
-    Car(String model, String color){
-        this(model,color,250);
+    public void setStop(boolean stop) {
+        this.stop = stop;
+        if(stop == true) this.speed=0;
     }
-
-    Car(String model, String color, int maxSpeed){
-        this.model = model;
-        this.color = color;
-        this.maxSpeed = maxSpeed;
-    }
+    //    int gas;
+//    String company = "현대자동차";
+//    String model;
+//    String color;
+//    int maxSpeed;
+//
+//    public Car() {
+//
+//    }
+//
+//    void setGas(int gas){
+//        this.gas = gas;
+//    }
+//
+//    boolean isLeftGas(){
+//        if(gas == 0){
+//            System.out.println("gas가 없다");
+//            return false;
+//        }
+//        System.out.println("gas있음");
+//        return true;
+//    }
+//
+//    void run(){
+//        while(true){
+//            if(gas > 0){
+//                System.out.println("달린다. 잔량 : "+ gas);
+//                gas-=1;
+//            }else{
+//                System.out.println("멈춘다. 잔량 : " + gas);
+//                return;
+//            }
+//        }
+//    }
+//
+//    Car(String model){
+//        this(model, "은색",250);
+//    }
+//
+//    Car(String model, String color){
+//        this(model,color,250);
+//    }
+//
+//    Car(String model, String color, int maxSpeed){
+//        this.model = model;
+//        this.color = color;
+//        this.maxSpeed = maxSpeed;
+//    }
 }
--- src/CarExample.java
+++ src/CarExample.java
@@ -2,13 +2,26 @@ public class CarExample {
     public static void main(String[] args) {
 
         Car myCar = new Car();
-        myCar.setGas(5);
 
-        if(myCar.isLeftGas()){
-            System.out.println("출발");
-            myCar.run();
+        myCar.setSpeed(-50);
+        System.out.println("현재 속도 : " + myCar.getSpeed());
+
+        myCar.setSpeed(60);
+        System.out.println("현재 속도 : " + myCar.getSpeed());
+
+        if(!myCar.isStop()){
+            myCar.setStop(true);
         }
-        System.out.println("gas주입하세요");
+        System.out.println("현재 속도 : " + myCar.getSpeed());
+
+//        Car myCar = new Car();
+//        myCar.setGas(5);
+//
+//        if(myCar.isLeftGas()){
+//            System.out.println("출발");
+//            myCar.run();
+//        }
+//        System.out.println("gas주입하세요");
 //        Car car1 = new Car("자가용");
 //        System.out.println(car1.company);
 //        System.out.println(car1.model);
