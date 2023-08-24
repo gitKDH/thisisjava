4f889a41bc7e6dea7af150fef83710fd7d137b7b
gitKDH <reg0212@naver.com>
2023-08-24 23:32:36 +0900

    chapter8

--- src/RemoteControl.java
+++ src/RemoteControl.java
@@ -1,3 +1,6 @@
 public interface RemoteControl {
     public void turnOn();
+
+    int MAX_VOLUME = 10;
+    int MIN_VOLUME = 0;
 }
--- src/RemoteControlExample.java
+++ src/RemoteControlExample.java
@@ -1,6 +1,14 @@
 public class RemoteControlExample {
     public static void main(String[] args) {
-        RemoteControl rc = new Television();
+        RemoteControl rc;
+
+        rc = new Television();
         rc.turnOn();
+
+        rc = new Audio();
+        rc.turnOn();
+
+        System.out.println("리모콘 최대 볼륨 : " + RemoteControl.MAX_VOLUME);
+        System.out.println("리모콘 최소 볼륨 : " + RemoteControl.MIN_VOLUME);
     }
 }
